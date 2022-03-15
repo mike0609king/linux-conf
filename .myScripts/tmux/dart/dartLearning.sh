ad2ea7f6eb5f03cf280b63b3d8ed2cc4a075a5f2
mike0609king <mike-huang@web.de>
2022-03-15 11:35:15 +0100

    VERY small cleanup

--- .myScripts/tmux/dart/dartLearning.sh
+++ .myScripts/tmux/dart/dartLearning.sh
@@ -3,23 +3,20 @@
 # Name of the session and names of the window
 # with additional indizes
 session="dart"
-
 PlanAndLog="$session:1"
 doc="$session:2"
 
 # Making the session
 tmux -2 new -d -s "$session"
-  #lines="$(tput lines)"
-  #columns="$(tput cols)"
-  #tmux -2 new-session -d -x "$columns" -y "$lines" -s "$session"
 
+# First Window is the window for Plan And Log
 tmux rename-window -t "$PlanAndLog" "P&L"
 tmux send-key -t "$PlanAndLog" \
 "cd '/media/sf_e/LearningAndDoc/GelernteSachenDoc/Informatik/Programmiersprachen/Dart/Plan and Log for learning/'" C-m \
 "clear" C-m \
 "vim 'Learning Dart.tex'" C-m 
 
-# Music window
+# Documentation
 tmux new-window -t "$doc" -n 'doc'
 tmux send-key -t "$doc" \
 "cd /media/sf_e/LearningAndDoc/GelernteSachenDoc/Informatik/Programmiersprachen/Dart/Documentation" C-m \
